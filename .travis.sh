ea6ff34aa5d2e72f968c71a9e010801739b6b1d8
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-18 16:44:19 +0100

    test for travis ticket 2003

--- .travis.sh
+++ .travis.sh
@@ -6,4 +6,14 @@ source ${SUPPORT_FIRECLOUD_DIR}/bin/common.inc.sh
 # function travis_run_<step>() {
 # }
 
+function travis_run_before_install() {
+    echo_skip "Test for Travis ticket #2003"
+
+    echo_info "The commands bellow should not terminate the debug session"
+    echo_info "after calling 'travis_run_before_install'..."
+
+    false
+    exit 1
+}
+
 source "${SUPPORT_FIRECLOUD_DIR}/repo/dot.travis.sh.sf"
