0bff5223d30273235f0ed8676ec03940fdfb3f03
Fredrik Öhrström <fredrik.ohrstrom@tobii.com>
2020-05-04 14:52:39 +0200

    Removed python2

--- ci/brew-install-py.inc.sh
+++ ci/brew-install-py.inc.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 
 echo_do "brew: Installing Python packages..."
 BREW_FORMULAE="$(cat <<-EOF
-python@2
-python@3
+python
 pyenv
 EOF
 )"
@@ -12,15 +11,13 @@ brew_install "${BREW_FORMULAE}"
 unset BREW_FORMULAE
 eval "$(pyenv init -)"
 mkdir -p ~/.pyenv/versions
-for f in $(brew --cellar python@2)/* $(brew --cellar python@3)/*; do
+for f in $(brew --cellar python)/*; do
     ln -sf $f ~/.pyenv/versions/
 done
 echo_done
 
 echo_do "brew: Testing Python packages..."
-exe_and_grep_q "python2 --version 2>&1 | head -1" "^Python 2\\."
 exe_and_grep_q "python3 --version 2>&1 | head -1" "^Python 3\\."
-exe_and_grep_q "pip2 --version | head -1" "^pip "
 exe_and_grep_q "pip3 --version | head -1" "^pip "
 exe_and_grep_q "pyenv --version | head -1" "^pyenv "
 echo_done
@@ -35,6 +32,6 @@ echo_done
 PIPENV_TAG=2549656dc09e132d8ba2fa6327c939f5f9a951b7
 echo_do "brew: Installing pipenv@${PIPENV_TAG} via pip3..."
 brew uninstall --force pipenv
-pip3 install git+https://github.com/pypa/pipenv.git@${PIPENV_TAG}
+pip3 install https://github.com/pypa/pipenv/archive/${PIPENV_TAG}.zip
 exe_and_grep_q "pipenv --version | head -1" "^pipenv, version 2018.11.27.dev0"
 echo_done
