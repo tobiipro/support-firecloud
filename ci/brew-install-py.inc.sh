e6d55e4041b499e55624618b4926c5cb8755a678
Fredrik Öhrström <fredrik.ohrstrom@tobii.com>
2020-05-07 12:39:45 +0200

    Remove pyenv.

--- ci/brew-install-py.inc.sh
+++ ci/brew-install-py.inc.sh
@@ -2,24 +2,12 @@
 set -euo pipefail
 
 echo_do "brew: Installing Python packages..."
-BREW_FORMULAE="$(cat <<-EOF
-python
-pyenv
-EOF
-)"
-brew_install "${BREW_FORMULAE}"
-unset BREW_FORMULAE
-eval "$(pyenv init -)"
-mkdir -p ~/.pyenv/versions
-for f in $(brew --cellar python)/*; do
-    ln -sf $f ~/.pyenv/versions/
-done
+brew_install python
 echo_done
 
 echo_do "brew: Testing Python packages..."
 exe_and_grep_q "python3 --version 2>&1 | head -1" "^Python 3\\."
 exe_and_grep_q "pip3 --version | head -1" "^pip "
-exe_and_grep_q "pyenv --version | head -1" "^pyenv "
 echo_done
 
 # FIXME temporary fix
