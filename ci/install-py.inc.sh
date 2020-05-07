353b57f5536baa959921d688883f2ccc537becab
Fredrik Öhrström <fredrik.ohrstrom@tobii.com>
2020-05-07 13:49:51 +0200

    Use apt properly.

--- ci/install-py.inc.sh
+++ ci/install-py.inc.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo_do "brew: Installing Python packages..."
-brew_install python
-echo_done
+if [[ "$OS" = "linux" ]] && [[ "${FORCE_BREW:-}" != "true" ]]; then
+    echo_do "apt: Installing Python3..."
+    apt_install python3
+    echo_done
+else
+    echo_do "brew: Installing Python packages..."
+    brew_install python
+    echo_done
+fi
 
-echo_do "brew: Testing Python packages..."
+echo_do "python: Testing Python packages..."
 exe_and_grep_q "python3 --version 2>&1 | head -1" "^Python 3\\."
 exe_and_grep_q "pip3 --version | head -1" "^pip "
 echo_done
