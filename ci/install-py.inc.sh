3bd4a3b77e77d54a7a8d7c58d309c8552de7212c
Fredrik Öhrström <fredrik.ohrstrom@tobii.com>
2020-05-08 12:04:04 +0200

    Remove pipenv

--- ci/install-py.inc.sh
+++ ci/install-py.inc.sh
@@ -17,17 +17,3 @@ echo_do "python: Testing Python packages..."
 exe_and_grep_q "python3 --version 2>&1 | head -1" "^Python 3\\."
 exe_and_grep_q "pip3 --version | head -1" "^pip "
 echo_done
-
-# FIXME temporary fix
-# See https://github.com/pypa/pipenv/issues/3395
-# See https://github.com/pypa/virtualenv/issues/1270
-
-# pipenv hasn't been released in a while, so we take a newer unreleased version straight from the git repo
-# https://github.com/pypa/pipenv/commit/2549656dc09e132d8ba2fa6327c939f5f9a951b7 was chosen
-# because it has a green CI run https://github.com/pypa/pipenv/runs/361861300
-PIPENV_TAG=2549656dc09e132d8ba2fa6327c939f5f9a951b7
-echo_do "brew: Installing pipenv@${PIPENV_TAG} via pip3..."
-brew uninstall --force pipenv
-pip3 install https://github.com/pypa/pipenv/archive/${PIPENV_TAG}.zip
-exe_and_grep_q "pipenv --version | head -1" "^pipenv, version 2018.11.27.dev0"
-echo_done
